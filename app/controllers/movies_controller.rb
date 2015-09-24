07099d401dd49c0fba891fa641dbea614d7077c7
Mallika Pokharel <mallikashree@tamu.edu>
2015-09-24 00:13:41 -0300

    hw2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,18 +6,39 @@ class MoviesController < ApplicationController
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    @movie = Movie.find(id) 
   end
 
 
   def index
     @movies = Movie.all
-        if(params[:sort].to_s == 'title')
-        @movies = @movies.sort_by{|m| m.title }
-        elsif(params[:sort].to_s == 'release')
-        @movies= @movies.sort_by{|m| m.release_date.to_s }
+
+
+    if(@ticked != nil)
+        @movies= @movies.find_all{ |m| @ticked.hs_key?(m.rating) and @ticked[m.rating]==true}
+    end
+   
+
+	if(params[:sort].to_s == 'title')
+	@movies = @movies.sort_by{|m| m.title }
+	elsif(params[:sort].to_s == 'release')
+	@movies= @movies.sort_by{|m| m.release_date.to_s }
+	end
+	if(params[:ratings] != nil)
+        @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
         end
+
+	@ticked = {}
+        @all_ratings = ['G','PG','PG-13','R']
+
+        @all_ratings.each { |rating|
+                if params[:ratings] == nil
+                @ticked[rating] = false
+                else
+                @ticked[rating] = params[:ratings].has_key?(rating)
+                end
+                }
+
   end
 
   def new
