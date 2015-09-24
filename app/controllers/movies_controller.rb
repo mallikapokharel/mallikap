49187546457af4493804d786f81acd50c44f3375
Mallika Pokharel <mallikashree@tamu.edu>
2015-09-24 00:29:58 -0300

    hw2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,47 +13,11 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-
-	@revert = 0
-    if(@ticked != nil)
-        @movies= @movies.find_all{ |m| @ticked.hs_key?(m.rating) and @ticked[m.rating]==true}
-    end
-   
-
-	if(params[:sort].to_s == 'title')
-	session[:sort] = params[:sort]
-	@movies = @movies.sort_by{|m| m.title }
-	elsif(params[:sort].to_s == 'release')
-	session[:sort] = params[:sort]
-	@movies= @movies.sort_by{|m| m.release_date.to_s }
-	elsif(session.has_key?(:sort) )
-        params[:sort] = session[:sort]
-        @revert = 1
-	end
-
-	if(params[:ratings] != nil)
-	session[:ratings] = params[:ratings]
-        @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
-	elsif(session.has_key?(:ratings) )
-        params[:ratings] = session[:ratings]
-	@revert = 1
+        if(params[:sort].to_s == 'title')
+        @movies = @movies.sort_by{|m| m.title }
+        elsif(params[:sort].to_s == 'release')
+        @movies= @movies.sort_by{|m| m.release_date.to_s }
         end
-
-	if(@revert ==1)
-        redirect_to movies_path(:sort=>params[:sort], :ratings =>params[:ratings] )
-	end
-
-	@ticked = {}
-        @all_ratings = ['G','PG','PG-13','R']
-
-        @all_ratings.each { |rating|
-                if params[:ratings] == nil
-                @ticked[rating] = false
-                else
-                @ticked[rating] = params[:ratings].has_key?(rating)
-                end
-                }
-
   end
 
   def new
